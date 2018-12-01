933d053918ebbda49310e16116675c5e3ebf0f8a
Dragon Dave McKee <dragon@scraperwiki.com>
2018-12-01 03:34:10 +0000

    Add most other data tables

--- data.js
+++ data.js
@@ -69,5 +69,39 @@ Survivor of Ukko	Suomenusko			Short Reign Penalty is Disabled Rulers can spend P
 Eternal Riders	Tengri								Polygamy is Allowed	No			Ignore Defensive Attrition Army Offensive Bonus		\n\
 Dawnbreakers	Zunist		Close-Kin Marriage amongst nobility is considered a Divine Union Reformer's family becomes Divine.						Polygamy is Allowed Close-Kin Marriage is Allowed	No					";
 
+unreformed_religion_csv = "Unreformed Religion	Superstitions	Succession Laws	Resistance to Proselytizing	Looting	Military	Priesthood	Male Priesthood	Female Priesthood	Increased Defensive Attrition	Prestige	Rulership	Concubines	Consorts	can_call_crusade	Short Reign	intermarry\n\
+African		Enables Eldership succession law.		Enabled	Armies are stronger when defending on home ground	Priesthood can marry. Priesthood may be female.	TRUE	TRUE	Yes			Yes	Yes			\n\
+Aztec				Disabled	Has increased levy size	Priesthood can marry.	TRUE			Prestige loss when at peace for too long.	Rulers do not receive opinion penalties for raised vassal levies.	Yes	No			\n\
+Bön			Yes	Enabled	Armies are stronger when defending on home ground	Priesthood cannot inherit. Priesthood may be female.	TRUE	TRUE	Yes		Rulers can choose to gain Sympathy with another faith by decisions.	Yes	Yes			indian_group zoroastrian_group nestorian_group\n\
+Germanic				Enabled	Prepared Invasions are allowed. Fleet can navigate through major rivers.  Ship Maintenance Reduced to 10%. Looters prefer coastal counties. Allows Viking traits/events Has increased levy size Has increased retinue size	Priesthood can marry. Priesthood may be female.	TRUE	TRUE	Yes	Prestige loss when at peace for too long.	Rulers do not receive opinion penalties for raised vassal levies.	Yes	No		2.5	norse_pagan_reformed baltic_pagan slavic_pagan\n\
+Hellenic	Character are assigned Zodiac traits on birth and by decision. Grants access to the Hermetic Society Diviner read the signs before a war to try to improve troop morale. Once per decade interpretation of stars by religious head or court chaplain if none exists			Disabled		Priesthood cannot marry. Priesthood cannot inherit. Priesthood may be female.	TRUE	TRUE				No	No			\n\
+Romuva		Enables Eldership succession law.	Yes	Enabled	Armies are stronger when defending on home ground	Priesthood can marry. Priesthood may be female.	TRUE	TRUE	Yes			Yes	No		2	norse_pagan slavic_pagan baltic_pagan_reformed\n\
+Slavic				Enabled	Armies are stronger when defending on home ground	Priesthood can marry. Priesthood may be female.	TRUE	TRUE	Yes			Yes	No		2	\n\
+Suomenusko				Enabled	Armies are stronger when defending on home ground	Priesthood can marry. Priesthood may be female.	TRUE	TRUE	Yes			Yes	No		2	\n\
+Tengri				Enabled	Armies ignore defensive attrition. Has increased levy size. Light cavalry are stronger	Priesthood can marry.	TRUE			Prestige loss when at peace for too long.	Rulers do not receive opinion penalties for raised vassal levies.	Yes	No		1.5	buddhist zoroastrian_group\n\
+Zunist	Grants access to the Hermetic Society			Disabled	Has increased levy size Heavy infantry are stronger	Priesthood can marry.	TRUE		Yes			Yes	No			indian_group zoroastrian_group";
+
+reformed_religion_csv = "Reformed Religion	Superstitions	Succession Laws	Resistance to Proselytizing	Looting	Military	Priesthood	Male Priesthood	Female Priesthood	Increased Defensive Attrition	Prestige	Rulership	Concubines	Consorts	Great Holy War	Short Reign	intermarry\n\
+African				Enabled		Priesthood can marry.	TRUE	TRUE				Yes	Yes	Enabled		\n\
+Aztec					Has increased levy size	Priesthood can marry.	TRUE					Yes	No	Enabled		\n\
+Bön			Yes	Enabled		Priesthood cannot inherit.	TRUE	TRUE	Yes			Yes	Yes	Enabled		indian_group zoroastrian_group nestorian_group\n\
+Germanic				Enabled	Has increased levy size	Priesthood can marry.	TRUE	TRUE				Yes	No	Enabled	1.5	norse_pagan\n\
+Hellenic	Character are assigned Zodiac traits on birth and by decision. Grants access to the Hermetic Society Diviner read the signs before a war to try to improve troop morale. Once per decade interpretation of stars by religious head or court chaplain if none exists					Priesthood cannot marry. Priesthood cannot inherit.	TRUE	TRUE				No	No	Enabled		\n\
+Romuva				Enabled		Priesthood can marry.	TRUE	TRUE				Yes	No	Enabled	1.5	baltic_pagan\n\
+Slavic				Enabled		Priesthood can marry.	TRUE	TRUE				Yes	No	Enabled	1.5	slavic_pagan\n\
+Suomenusko				Enabled		Priesthood can marry.	TRUE	TRUE				Yes	No	Enabled	1.5	\n\
+Tengri				Enabled	Has increased levy size. Light cavalry are stronger	Priesthood can marry.	TRUE					Yes	No	Enabled	1.25	buddhist zoroastrian_group\n\
+Zunist	Grants access to the Hermetic Society					Priesthood can marry.	TRUE					Yes	No	Enabled		indian_group zoroastrian_group";
+
+leadership_csv = "Leadership	Religious Head	Superstitions	Great Holy War	Attribute Bonus\n\
+Autocephalous	Each realm has it own religious head			\n\
+Autonomous	Can never have a religious head.	Followers can pick a Branch	Disabled	Autonomous\n\
+Hierocratic	A priest is put in charge of this religion.			\n\
+Temporal	Reformer becomes religious head. Religious head is a secular title.			";
+
+
 natures = csvJSON(natures_csv);
 doctrine = csvJSON(doctrine_csv);
+unreformed_religion = csvJSON(unreformed_religion_csv);
+reformed_religion = csvJSON(reformed_religion_csv);
+leadership = csvJSON(leadership_csv);
